65278f99e3be081c4a8333c9b66cad4d9bc09b7a
timonrieger <github.com.kindle922@passmail.net>
2025-01-06 19:56:58 +0100

    fix: revert polymorphic relationships in User class (did not get it to work)

--- database/app.py
+++ database/app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import json
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Float, DateTime, Text, func, Boolean
 from typing import List
 from flask_migrate import Migrate
@@ -20,11 +20,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Base(DeclarativeBase):
-    pass
 
-
-class AirNomads(Base):
+class AirNomads(db.Model):
     __tablename__ = "air_nomads"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String)
@@ -52,7 +49,7 @@ class AirNomads(Base):
         }
 
 
-class TopMovies(Base):
+class TopMovies(db.Model):
     __tablename__ = "top_movies"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[str] = mapped_column(Integer, db.ForeignKey("users.id"))
@@ -75,7 +72,7 @@ class TopMovies(Base):
         }
 
 
-class User(Base):
+class User(db.Model):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(150), unique=True)
@@ -85,14 +82,6 @@ class User(Base):
     token: Mapped[str] = mapped_column(String, unique=True)
     admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
     apikey: Mapped[str] = mapped_column(String, nullable=True)
-    blog_posts: Mapped[List["BlogPost"]] = relationship("BlogPost", back_populates="author")
-    blog_comments: Mapped[List["BlogComment"]] = relationship("BlogComment", back_populates="comment_author")
-    type: Mapped[str] = mapped_column(String(50), default=os.urandom(50))
-
-    __mapper_args__ = {
-        'polymorphic_identity': __tablename__,
-        "polymorphic_on": "type"
-    }
 
     def to_dict(self):
         return {
@@ -104,12 +93,10 @@ class User(Base):
             "token": self.token,
             "admin": self.admin,
             "apikey": self.apikey,
-            "blog_posts": self.blog_posts,
-            "blog_comments": self.blog_comments
         }
         
         
-class Ressources(Base):
+class Ressources(db.Model):
     __tablename__ = "ressources"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
@@ -137,7 +124,7 @@ class Ressources(Base):
         }
 
 
-class BlogPost(Base):
+class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -147,18 +134,16 @@ class BlogPost(Base):
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
     # Relationships
     author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
-    author: Mapped["User"] = relationship("User", back_populates="blog_posts")
     comments: Mapped[List["BlogComment"]] = relationship("BlogComment", back_populates="parent_post")
 
 
-class BlogComment(Base):
+class BlogComment(db.Model):
     __tablename__ = "blog_comments"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(String, nullable=False)
     time: Mapped[str] = mapped_column(String, nullable=False)
     # Relationships
     author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
-    comment_author: Mapped["User"] = relationship("User", back_populates="blog_comments")
     post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
     parent_post: Mapped["BlogPost"] = relationship("BlogPost", back_populates="comments")
 
